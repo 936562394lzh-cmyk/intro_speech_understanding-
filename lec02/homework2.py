c6789fd93e70906499f6457f0d51bf9fa0082e89
936562394lzh-cmyk <936562394lzh@gmail.com>
2026-04-22 10:51:06 +0900

    Enhance arithmetic function for type-based operations
    Refactor arithmetic function to handle multiple types and operations.

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -2,15 +2,29 @@
 This homework defines one method, called "arithmetic".
 that method, type `help homework2.arithmetic`.
 '''
-
 def arithmetic(x, y):
-    """
+  """
+    Perform operations based on types of x and y:
     Modify this code so that it performs one of four possible functions, 
+    - str + str: concatenation
     as specified in the following table:
-
+    - str * int(y): repeat string
+    - str(x) + str: convert float to string then concatenate
                         isinstance(x,str)  isinstance(x,float)
+    - float * float: multiplication
     isinstance(y,str)   return x+y         return str(x)+y
+    if isinstance(x, str) and isinstance(y, str):
     isinstance(y,float) return x*int(y)    return x*y
+        return x + y
     """
+    if isinstance(x, str) and isinstance(y, str):
+        return x + y
+    elif isinstance(x, str) and isinstance(y, float):
+        return x * int(y)
+    elif isinstance(x, float) and isinstance(y, str):
+        return str(x) + y
+    elif isinstance(x, float) and isinstance(y, float):
+        return x * y
+    else:
+        raise TypeError("Unsupported operand types")
     return 0
-
