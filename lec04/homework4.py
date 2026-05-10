c5d8d3e1fc6ba66b333ad828b535f3a7ab298f6b
936562394lzh-cmyk <936562394lzh@gmail.com>
2026-05-11 00:53:56 +0900

    Update homework4.py

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,4 +1,51 @@
-birthdays = {(1,1): ["Alice"], (12,31): ["Bob"]}
-print(next_birthday((12,30), birthdays))   # ((12,31), ["Bob"])
-print(next_birthday((12,31), birthdays))   # ((1,1), ["Alice"])
-print(next_birthday((1,2), birthdays))     # ((1,1), ["Alice"])  跨年
+def next_birthday(date, birthdays):
+    '''
+    Find the next birthday after the given date.
+
+    @param:
+    date - a tuple of two integers specifying (month, day)
+    birthdays - a dict mapping from date tuples to lists of names, for example,
+      birthdays[(1,10)] = list of all people with birthdays on January 10.
+
+    @return:
+    birthday - the next day, after given date, on which somebody has a birthday
+    list_of_names - list of all people with birthdays on that date
+    '''
+  
+    if not birthdays:
+        return (1, 1), [] 
+
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def day_of_year(month, day):
+      
+        return sum(month_days[:month-1]) + day
+
+    given_day = day_of_year(date[0], date[1])
+
+    next_day = None      
+    next_date = None    
+    next_names = None    
+
+    min_day = None      
+    min_date = None      
+    min_names = None    
+
+    for bdate, names in birthdays.items():
+        bday = day_of_year(bdate[0], bdate[1])
+
+        if min_day is None or bday < min_day:
+            min_day = bday
+            min_date = bdate
+            min_names = names
+
+        if bday > given_day:
+            if next_day is None or bday < next_day:
+                next_day = bday
+                next_date = bdate
+                next_names = names
+
+    if next_date is not None:
+        return next_date, next_names
+    else:
+        return min_date, min_names
