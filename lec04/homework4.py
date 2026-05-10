121f721ce3ec896422ae4514090c2367c01e9040
936562394lzh-cmyk <936562394lzh@gmail.com>
2026-05-11 00:50:09 +0900

    Add example usage for next_birthday function
    Updated the next_birthday function to include example usage.

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,18 +1,4 @@
-
-def next_birthday(date, birthdays):
-    '''
-    Find the next birthday after the given date.
-
-    @param:
-    date - a tuple of two integers specifying (month, day)
-    birthdays - a dict mapping from date tuples to lists of names, for example,
-      birthdays[(1,10)] = list of all people with birthdays on January 10.
-
-    @return:
-    birthday - the next day, after given date, on which somebody has a birthday
-    list_of_names - list of all people with birthdays on that date
-    '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
-    
+birthdays = {(1,1): ["Alice"], (12,31): ["Bob"]}
+print(next_birthday((12,30), birthdays))   # ((12,31), ["Bob"])
+print(next_birthday((12,31), birthdays))   # ((1,1), ["Alice"])
+print(next_birthday((1,2), birthdays))     # ((1,1), ["Alice"])  跨年
